ce0bb137f4197e11be6d7ca6c8e5b1508d8287df
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2023-04-13 18:01:17 +0100

    Initial Commit

---
+++ src/components/AllInOne.jsx
@@ -0,0 +1,92 @@
+import { CheckIcon } from "@heroicons/react/24/outline"
+
+
+const AllInOne = () => {
+  return (
+    <div name='allinone' className="w-full my-32">
+        <div className="max-w-[1240px] mx-auto px-2">
+            <h2 className="text-5xl font-bold text-center">All-In-One Platform</h2>
+            <p className="text-2xl py-8 text-gray-500 text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto reiciendis ducimus maxime quos similique voluptatum atque officiis cumque officia non inventore illo, ad quo quis pariatur recusandae vel qui? Minima!</p>
+
+            <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+                
+                <div className="flex">
+                    <div>
+                        <CheckIcon className="w-7 mr-4 text-green-600" />
+                    </div>
+                    <div className="">
+                        <h3 className="font-bold text-lg">Notifications</h3>
+                        <p className="text-lg pt-2 pb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi soluta molestias illo velit! Facilis voluptatum non dolorem sed omnis, modi culpa enim quam mollitia voluptatibus eius iusto ab repellat delectus.</p>
+                    </div>
+                </div>
+                <div className="flex">
+                    <div>
+                        <CheckIcon className="w-7 mr-4 text-green-600" />
+                    </div>
+                    <div className="">
+                        <h3 className="font-bold text-lg">Notifications</h3>
+                        <p className="text-lg pt-2 pb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi soluta molestias illo velit! Facilis voluptatum non dolorem sed omnis, modi culpa enim quam mollitia voluptatibus eius iusto ab repellat delectus.</p>
+                    </div>
+                </div>
+                <div className="flex">
+                    <div>
+                        <CheckIcon className="w-7 mr-4 text-green-600" />
+                    </div>
+                    <div className="">
+                        <h3 className="font-bold text-lg">Notifications</h3>
+                        <p className="text-lg pt-2 pb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi soluta molestias illo velit! Facilis voluptatum non dolorem sed omnis, modi culpa enim quam mollitia voluptatibus eius iusto ab repellat delectus.</p>
+                    </div>
+                </div>
+                <div className="flex">
+                    <div>
+                        <CheckIcon className="w-7 mr-4 text-green-600" />
+                    </div>
+                    <div className="">
+                        <h3 className="font-bold text-lg">Notifications</h3>
+                        <p className="text-lg pt-2 pb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi soluta molestias illo velit! Facilis voluptatum non dolorem sed omnis, modi culpa enim quam mollitia voluptatibus eius iusto ab repellat delectus.</p>
+                    </div>
+                </div>
+                <div className="flex">
+                    <div>
+                        <CheckIcon className="w-7 mr-4 text-green-600" />
+                    </div>
+                    <div className="">
+                        <h3 className="font-bold text-lg">Notifications</h3>
+                        <p className="text-lg pt-2 pb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi soluta molestias illo velit! Facilis voluptatum non dolorem sed omnis, modi culpa enim quam mollitia voluptatibus eius iusto ab repellat delectus.</p>
+                    </div>
+                </div>
+                <div className="flex">
+                    <div>
+                        <CheckIcon className="w-7 mr-4 text-green-600" />
+                    </div>
+                    <div className="">
+                        <h3 className="font-bold text-lg">Notifications</h3>
+                        <p className="text-lg pt-2 pb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi soluta molestias illo velit! Facilis voluptatum non dolorem sed omnis, modi culpa enim quam mollitia voluptatibus eius iusto ab repellat delectus.</p>
+                    </div>
+                </div>
+                <div className="flex">
+                    <div>
+                        <CheckIcon className="w-7 mr-4 text-green-600" />
+                    </div>
+                    <div className="">
+                        <h3 className="font-bold text-lg">Notifications</h3>
+                        <p className="text-lg pt-2 pb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi soluta molestias illo velit! Facilis voluptatum non dolorem sed omnis, modi culpa enim quam mollitia voluptatibus eius iusto ab repellat delectus.</p>
+                    </div>
+                </div>
+                <div className="flex">
+                    <div>
+                        <CheckIcon className="w-7 mr-4 text-green-600" />
+                    </div>
+                    <div className="">
+                        <h3 className="font-bold text-lg">Notifications</h3>
+                        <p className="text-lg pt-2 pb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi soluta molestias illo velit! Facilis voluptatum non dolorem sed omnis, modi culpa enim quam mollitia voluptatibus eius iusto ab repellat delectus.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AllInOne
